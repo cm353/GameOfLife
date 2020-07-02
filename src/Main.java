f2a2cc486d6cf0e9c9715ae9001e183bf92ad0d2
cm353 <christian.meyenburg@gmail.com>
2020-07-02 11:46:07 +0100

    Refactored Exception handling in Main

--- src/Main.java
+++ src/Main.java
@@ -3,55 +3,61 @@ import javax.imageio.stream.FileImageOutputStream;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 /**
- *
  * Uses GifSequenceWriter
  * Uses GameOfLife
- *
  */
 
 public class Main {
 
-	static File path;
-	static {
-		path = new File("G:/images");
-		path.mkdirs();
-	}
+    static File path;
 
-	public static void main(String[] args) throws Exception {
+    static {
+        path = new File("G:/images");
+        path.mkdirs();
+    }
 
-		int column = 1024;
-		int rows = 768;
-		int numberOfIterations = 100;
-		
-		GameOfLife.execute(column, rows, numberOfIterations);
+    public static void main(String[] args) {
 
-		GifSequenceWriter writer=null;
-		ImageOutputStream output=null;
+        int column = 1024;
+        int rows = 768;
+        int numberOfIterations = 100;
+        GameOfLife.execute(column, rows, numberOfIterations);
+        GifSequenceWriter writer = null;
+        ImageOutputStream output = null;
+        String pathname = "gol" + 0 + ".gif";
 
-			String pathname = "gol" + 0 + ".gif";
+        try {
+            // grab the output image type from the first image in the sequence
+            BufferedImage firstImage = ImageIO.read(new File(path, pathname));
 
-			// grab the output image type from the first image in the sequence
-			BufferedImage firstImage = ImageIO.read(new File(path,pathname));
+            // create a new BufferedOutputStream with the last argument
+            output =
+                    new FileImageOutputStream(new File(path, "Test.gif"));
+            // create a gif sequence with the type of the first image, 1 second
+            // between frames, which loops continuously
+            writer =
+                    new GifSequenceWriter(output, firstImage.getType(), 1, false);
 
-			// create a new BufferedOutputStream with the last argument
-			 output =
-					new FileImageOutputStream(new File(path, "Test.gif"));
-			// create a gif sequence with the type of the first image, 1 second
-			// between frames, which loops continuously
-			 writer =
-					new GifSequenceWriter(output, firstImage.getType(), 1, false);
-
-			// write out the first image to our sequence...
-			writer.writeToSequence(firstImage);
-			for (int i = 1; i <= numberOfIterations; i++) {
-				pathname = "gol" + i + ".gif";
-				BufferedImage nextImage = ImageIO.read(new File(path,pathname));
-				writer.writeToSequence(nextImage);
-				System.out.println("Gif Sequence item No.:" + i);
-			}
-		writer.close();
-		output.close();
-	}
+            // write out the first image to our sequence...
+            writer.writeToSequence(firstImage);
+            for (int i = 1; i <= numberOfIterations; i++) {
+                pathname = "gol" + i + ".gif";
+                BufferedImage nextImage = ImageIO.read(new File(path, pathname));
+                writer.writeToSequence(nextImage);
+                System.out.println("Gif Sequence item No.:" + i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+                output.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
